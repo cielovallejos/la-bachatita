dec572d42a787a486fc9716f0f66328089c77baf
cielovallejos <a.cielovallejos@icea.edu.ar>
2024-10-08 18:12:24 +0000

    m

--- cielovallejos.py
+++ cielovallejos.py
@@ -1,1 +1,13 @@
-.
+valor='cielo'
+cielo="."
+nombre='cielo'
+if(2+2)==4:
+    print("nombre")
+def nombre(argumento):
+    print(argumento)
+    hola="a"
+    hola="b"
+while (nombre==valor):
+    print('hola')
+    nombre('hola')
+
